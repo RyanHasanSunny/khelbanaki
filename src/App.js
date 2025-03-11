e4d711d532db1b81c99022dbec6330c6c73b34bd
Ryan Hasan Sunny <132351176+RyanHasanSunny@users.noreply.github.com>
2025-03-12 05:52:37 +0600

    Dev (#21)
    * Faysal dev (#8)
    
    * Create home.js
    
    * Create hello.js
    
    ---------
    
    Co-authored-by: Faysal Hossain Tomal <faysaltomal02@gmail.com>
    
    * Ryan dev (#10)
    
    * Update App.js
    
    * Create flash.js
    
    * Create homepage.js
    
    * update
    
    * update dev from master (#11)
    
    * Update App.js
    
    * Create flash.js
    
    * Create homepage.js
    
    * update
    
    * update v1
    
    * landing page components added
    
    * Create server.js
    
    * Create player-gig.js
    
    * Create home.js
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    Co-authored-by: Faysal Hossain Tomal <faysaltomal02@gmail.com>
    
    * update dev (#17)
    
    * Update App.js
    
    * Create flash.js
    
    * Create homepage.js
    
    * update
    
    * update v1
    
    * landing page components added
    
    * Create server.js
    
    * Create player-gig.js
    
    * Create home.js
    
    * Shovon dev (#13) (#14)
    
    * update v1
    
    * landing page components added
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    
    * Shovon dev (#16)
    
    login page added
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    Co-authored-by: Faysal Hossain Tomal <faysaltomal02@gmail.com>
    
    * Ryan dev (#20)
    
    * Update App.js
    
    * Create flash.js
    
    * Create homepage.js
    
    * update
    
    * Shovon dev (#13)
    
    * update v1
    
    * landing page components added
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    
    * update ryan_dev (#18)
    
    * update v1
    
    * landing page components added
    
    * Create server.js
    
    * Create player-gig.js
    
    * Create home.js
    
    * Shovon dev (#13) (#14)
    
    * update v1
    
    * landing page components added
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    
    * Shovon dev (#16)
    
    login page added
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    Co-authored-by: Faysal Hossain Tomal <faysaltomal02@gmail.com>
    
    * login page bug fixed
    
    ---------
    
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>
    Co-authored-by: Faysal Hossain Tomal <faysaltomal02@gmail.com>
    
    ---------
    
    Co-authored-by: Faysal Hossain Tomal <faysaltomal02@gmail.com>
    Co-authored-by: K M Monoarul Islam Shovon <kmshovon2022@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./components/front-end/login-page/LoginPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
 
-          
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App flex justify-center items-center h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
